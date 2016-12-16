d63fdcca3afe13c2c8ad4e70ee0e36493af775aa
최성근 <ccc3132@naver.com>
2016-12-16 10:11:55 +0900

    2. [예약시작] 버튼을 클릭하면 그 위의 Chronometer가 작동한다. 3. Chronomter가 작동하면 "예약에 걸린 시간"의 색상이 빨간색으로 변경된다.

--- app/src/main/java/com/example/ss/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ss/myapplication/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.ss.myapplication;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.Chronometer;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    void init(){
+        final Chronometer c1;
+        final Button b1;
+        final CalendarView C1;
+        final TimePicker t1;
+
+
+        c1 = (Chronometer) findViewById(R.id.chronometer3);
+        b1 = (Button) findViewById(R.id.button);
+        C1 = (CalendarView) findViewById(R.id.calendarView);
+        t1 = (TimePicker) findViewById(R.id.timePicker);
+
+        C1.setVisibility(View.INVISIBLE);
+        t1.setVisibility(View.INVISIBLE);
+
+        b1.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v){
+                c1.setBase(SystemClock.elapsedRealtime());
+                c1.start();
+               c1.setTextColor(Color.RED);
+            }
+        });
+
+
+
+
+
     }
 }
