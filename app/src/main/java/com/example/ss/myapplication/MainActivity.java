28b76c64e4a8b330d6550d846ae1cc7e936d9980
최성근 <ccc3132@naver.com>
2016-12-16 10:16:20 +0900

    4, 날짜 설정을 선택하면 캘린더뷰가 보이고

--- app/src/main/java/com/example/ss/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ss/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.Chronometer;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,12 +26,19 @@ public class MainActivity extends AppCompatActivity {
         final Button b1;
         final CalendarView C1;
         final TimePicker t1;
-
+        final RadioGroup Ra1;
+        final RadioButton r1;
+        final RadioButton r2;
 
         c1 = (Chronometer) findViewById(R.id.chronometer3);
         b1 = (Button) findViewById(R.id.button);
         C1 = (CalendarView) findViewById(R.id.calendarView);
         t1 = (TimePicker) findViewById(R.id.timePicker);
+        Ra1 = (RadioGroup) findViewById(R.id.radiogroup);
+        r1 = (RadioButton) findViewById(R.id.radioButton);
+        r2 = (RadioButton) findViewById(R.id.radioButton2);
+
+
 
         C1.setVisibility(View.INVISIBLE);
         t1.setVisibility(View.INVISIBLE);
@@ -40,6 +49,16 @@ public class MainActivity extends AppCompatActivity {
                 c1.setBase(SystemClock.elapsedRealtime());
                 c1.start();
                c1.setTextColor(Color.RED);
+            }
+        });
+
+        Ra1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(r1.isChecked()){
+                    C1.setVisibility(View.VISIBLE);
+                    t1.setVisibility(View.GONE);
+                }
             }
         });
 
